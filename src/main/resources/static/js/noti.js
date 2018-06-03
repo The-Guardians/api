bc688836d03fc488f41c1f55373d21262a69ea00
MikuMiku <34748866+Wasin007@users.noreply.github.com>
2018-06-03 14:56:31 +0700

    add notibox 5 noti

--- src/main/resources/static/js/noti.js
+++ src/main/resources/static/js/noti.js
@@ -175,12 +175,12 @@ function removeNotibox(id) {
 
 function notifyMe() {
     // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-    }
+    // if (!("Notification" in window)) {
+    //     alert("This browser does not support desktop notification");
+    // }
 
     // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
+     if (Notification.permission === "granted") {
         // If it's okay let's create a notification
         var notification = new Notification("You have " + numnoti + " message in notify.");
     }
